d0e6dbd4dd892444c3a257698d3d040cfc41a07b
Oleh Speka <oleh.speka@socprime.com>
2022-04-18 11:48:26 +0300

    fix: empty config 2

--- Auth0Connector/main.py
+++ Auth0Connector/main.py
@@ -49,7 +49,7 @@ def main():
     if config_string:
         config = json.loads(config_string)
     else:
-        config = '{"last_log_id": "","last_date": ""}'
+        config = json.loads('{"last_log_id": "","last_date": ""}')
     logging.info(f'Config loaded\n\t{config}')
     connector = Auth0Connector(DOMAIN, API_PATH, CLIENT_ID, CLIENT_SECRET, AUDIENCE)
     last_log_id, events = connector.get_log_events(config)
